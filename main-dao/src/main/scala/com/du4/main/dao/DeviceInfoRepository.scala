eb742c6aacb2ccf51719f193fea09affc96a1edd
Vladimir Korchik <vagenius23@gmail.com>
2020-01-10 17:43:46 +0100

    DeviceInfoResitory first impl

--- main-dao/src/main/scala/com/du4/main/dao/DeviceInfoRepository.scala
+++ main-dao/src/main/scala/com/du4/main/dao/DeviceInfoRepository.scala
@@ -1,12 +1,16 @@
 package com.du4.main.dao
 
 import com.du4.db.DB
-import com.du4.tables.slick.generated.Tables.DeviceinfoRow
+import com.du4.tables.slick.generated.Tables._
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 trait DeviceInfoRepository extends DB {
 
-  def getAll(): Future[List[DeviceinfoRow]] = ???
+  import config.api._
+
+  implicit val ec: ExecutionContext
+
+  def getAll(): Future[List[DeviceinfoRow]] = db.run(Deviceinfo.result).map(_.toList)
 
 }
